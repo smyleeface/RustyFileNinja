0be32210ea2cadd405aac99fb9815ff133e8f63f
Patty R. <smyleeface@users.noreply.github.com>
2024-08-29 15:25:44 -0700

    refactor: add utils module and add prompt_for_required_value function

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,5 @@ pub mod create_file;
 pub mod copy_file;
 pub mod delete_file;
 pub mod combine_files;
+
+pub(crate) mod utils;
---
+++ src/utils/mod.rs
@@ -0,0 +1,13 @@
+use std::io::{stdin, stdout, Write, BufRead};
+
+/// Returns an input provided by the user.
+pub(crate) fn prompt_for_required_value(message_prompt: String) -> String {
+    let mut source_file = String::new();
+    while source_file.is_empty() {
+        print!("{}", message_prompt);
+        stdout().flush().unwrap();
+        stdin().read_line(&mut source_file).unwrap();
+        source_file = source_file.trim().to_string();
+    }
+    source_file
+}

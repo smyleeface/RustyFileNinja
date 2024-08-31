758936c9263ad9c96310121597b7e24cb34d1555
Patty R. <smyleeface@users.noreply.github.com>
2024-08-30 18:12:40 -0700

    feat: add copy command

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,5 @@
 use clap::{Parser, Subcommand};
-use fini::create_file;
+use fini::{create_file,copy_file};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -13,6 +13,8 @@ struct Cli {
 enum Commands {
     /// Creates a new file with or without content
     Create { name_of_file: Option<String>, content: Option<String> },
+    // Copies a file to another location when given both the source and destination
+    Copy { source_file: Option<String>, destination_location: Option<String> }
 }
 
 pub(crate) fn main() {
@@ -21,9 +23,16 @@ pub(crate) fn main() {
         Commands::Create { name_of_file, content } => {
             create_file::run(
                 name_of_file.clone().unwrap_or_else(|| "".to_string()),
-                content.clone().unwrap_or_else(|| "".to_string()))
-                .expect("Error running create_file command");
+                content.clone().unwrap_or_else(|| "".to_string())
+            )
+            .expect("Error running create_file command");
         }
-        _ => println!("Command not supported")
+        Commands::Copy { source_file, destination_location } => {
+            copy_file::run(
+                source_file.clone().unwrap_or_else(|| "".to_string()),
+                destination_location.clone().unwrap_or_else(|| "".to_string())
+            )
+            .expect("Error running copy_file command")
+        }
     }
 }

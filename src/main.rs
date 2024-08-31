005445e9e21e679b9f9b51455079962ad78a8c82
Patty R. <smyleeface@users.noreply.github.com>
2024-08-30 18:28:32 -0700

    refactor: remove old commands

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
-mod commands;
+mod cli;
 
 use std::io::{self};
-use std::str::FromStr;
-use std::env;
 
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let mut command= "help";
-    if args.len() > 1 {
-        command = args.get(1).unwrap();
-    }
-    let command_enum = commands::Commands::from_str(command).unwrap();
-    command_enum.run();
+    cli::main();
     Ok(())
 }

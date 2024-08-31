f686a753155f21ac5ab3de5056b2899e993618b7
Patty R. <smyleeface@users.noreply.github.com>
2024-08-30 18:12:19 -0700

    refactor: use prompt_input from utils

--- src/create_file/mod.rs
+++ src/create_file/mod.rs
@@ -3,22 +3,10 @@ use crate::utils;
 
 /// Begins the create file process.
 pub fn run(mut name_of_file: String, mut content: String) -> io::Result<()> {
-    name_of_file = prompt_input(name_of_file, String::from("\nName of file: "), true);
-    content = prompt_input(content, String::from("Content to write to the file: \n"), false);
+    name_of_file = utils::prompt_input(name_of_file, String::from("\nName of file: "), true);
+    content = utils::prompt_input(content, String::from("Content to write to the file: \n"), false);
     create_file(name_of_file, content)?;
     Ok(())
-}
-
-/// Returns the input provided by the user.
-fn prompt_input(value: String, mut prompt: String, required: bool) -> String {
-    if !value.is_empty() {
-        return value
-    }
-    if required {
-        utils::prompt_for_required_value(String::from(prompt))
-    } else {
-        utils::prompt_for_value(String::from(prompt))
-    }
 }
 
 /// Creates a file given the filename and content

89755addf36174f866a0551e4f9aaf6f61f4c3d8
Patty R. <smyleeface@users.noreply.github.com>
2024-09-02 19:01:06 -0700

    feat: add prompt for new location

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,7 +1,6 @@
 pub(crate) mod file_io;
 
 use std::io::{stdin, stdout, Write};
-use crate::utils;
 
 /// Returns an input provided by the user. Continues to prompt until a user enter a value.
 pub(crate) fn prompt_for_required_value(message_prompt: String) -> String {
@@ -37,12 +36,24 @@ pub(crate) fn prompt_input(value: String, prompt: String, required: bool) -> Str
 }
 
 pub fn prompt_to_overwrite(name_of_file: String) -> bool {
-    if file_io::file_exists(name_of_file) {
+    if file_io::file_exists(name_of_file.clone()) {
         let mut overwrite_ok = String::new();
-        overwrite_ok = utils::prompt_input(overwrite_ok, String::from("\nFile exists. Overwrite (Y/y)?  "), true);
+        overwrite_ok = prompt_input(overwrite_ok, format!("\nFile '{}' exists. Overwrite (Y/y)?  ", name_of_file), true);
         if overwrite_ok != "Y" && overwrite_ok != "y" {
             return false;
         }
     }
     true
+}
+
+pub fn prompt_for_new_location(mut name_of_file: String, prompt: String) -> String {
+    let mut does_file_exist = file_io::file_exists(name_of_file.clone());
+    while !does_file_exist {
+        let mut another_source_value = String::new();
+        println!("\nFile doesn't exist {}", name_of_file.clone());
+        another_source_value = prompt_input(another_source_value, prompt.clone(), true);
+        does_file_exist = file_io::file_exists(another_source_value.clone());
+        name_of_file = another_source_value;
+    }
+    name_of_file
 }

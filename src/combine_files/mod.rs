e6f931016ccf20d8e48f9acca216184f75358b68
Patty R. <smyleeface@users.noreply.github.com>
2024-09-02 18:29:44 -0700

    refactor: use prompt_to_overwrite on created values

--- src/combine_files/mod.rs
+++ src/combine_files/mod.rs
@@ -1,4 +1,5 @@
 use std::io::{self};
+use std::process::exit;
 use crate::utils;
 
 /// Begins the combine file process.
@@ -6,6 +7,9 @@ pub fn run(source_file_1: String, source_file_2: String, destination_file: Strin
     let source_file_1 = utils::prompt_input(source_file_1, String::from("\nFirst File: "), true);
     let source_file_2 = utils::prompt_input(source_file_2, String::from("\nSecond File: "), true);
     let destination_file = utils::prompt_input(destination_file, String::from("\nFile destination: "), true);
+    if !utils::prompt_to_overwrite(destination_file.clone()) {
+        exit(0)
+    }
     combine_files(source_file_1, source_file_2, destination_file)?;
     Ok(())
 }
